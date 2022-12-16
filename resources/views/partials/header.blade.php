9b08f5826c406142cb58e561158809e8e113ec5f
Omar-Codess <omartaki8@gmail.com>
2022-12-16 09:11:43 +0100

    creati e importati file per scss header e footer

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <span>DC POWER VISA</span>
+    <span>ADDITIONAL DC SITES</span>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis consectetur id dignissimos fuga,
+                cupiditate possimus quasi pariatur cum quo vero facere magnam vitae quam suscipit fugiat deleniti! At,
+                distinctio excepturi?</p>
+        </div>
+    </div>
+</div>
